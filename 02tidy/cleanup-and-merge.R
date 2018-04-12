19a3c1435478995bfe62c23189e8a41315723bab
Mark van der Loo <mark.vanderloo@gmail.com>
2018-04-12 12:00:35 +0200

    stuff

--- 02tidy/cleanup-and-merge.R
+++ 02tidy/cleanup-and-merge.R
@@ -34,13 +34,23 @@ edges %<>%  filter(str_trim(str_to_lower(Treinsoort)) %in% c("sprinter","stoptre
 # Merge the tarif distances ----
 # distance to long form, conversion of type
 distance <-  distmat %>% 
-  gather(EndCode, Distance, -Station, -naam) %>% 
+  gather(EndCode, Distance, -Station) %>% 
   rename(StartCode=Station) %>%
   mutate(Distance = as.numeric(Distance)) %>%
   filter(complete.cases(.))
 
 # merge full station names
 stationsnamen <- stations %>% select(code, naam)
+stationsnamen %<>% mutate(
+  naam=str_replace(naam,"a/d","aan den")
+  ,naam=str_replace(naam," v "," van ")) 
+# we add 'Zwolle Stadshage by hand'
+stationsnamen <- rbind(stationsnamen
+  , data.frame(
+      code=c("zls","bsks")
+    , naam=c("Zwolle Stadshagen","Boskoop Snijdelwijk")))
+
+
 dd <- left_join(distance, stationsnamen, by=c("StartCode"="code")) %>%
   rename(Start=naam)
 
@@ -48,7 +58,27 @@ dd <- left_join(dd, stationsnamen,by=c("EndCode"="code")) %>%
   rename(End=naam)
 
 # merge edges with distances
-out <- left_join(edges,dd)
+
+pattren <- c(" +\\(.*\\)", "-", "St\\.")
+replace <- c(""          , " ", "St")
+
+tm <- edges %>%
+  mutate(Start = str_trim(str_replace(Start, pattern, replace))
+         ,End  = str_trim(str_replace(End, pattern, replace))) %>%
+  filter(!Start %in% c("Onbekend","2018"))
+
+
+
+library(stringdist)
+
+j_start <- amatch(str_trim(tm$Start), str_trim(stations$naam), method='osa',maxDist = 2)
+View(tm[which(is.na(j_start)),])
+
+
+out <- left_join(tm,dd)
+head(out)
+i <- which(is.na(out$StartCode))
+View(out[i,])
 
 ## prepare output and write ----
 # We still have double entries becuse multiple train series can pass through the same stations.
